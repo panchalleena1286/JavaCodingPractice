8c820d104728cd34945fa06fdd7305ba9742311b
panchalleena1286 <panchalleena8839@gmail.com>
2022-03-31 23:51:36 +0530

    Day2_challenge

---
+++ HackerRank_30_days_challenge/Day-2/solution/Question_2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Question_2 {
+	public static void main(String args[]) {
+	 int i = 4;
+	   double d = 4.0;
+	   String s = "HackerRank ";
+
+
+	    Scanner scan = new Scanner(System.in);
+	        int a ;
+	        double b;
+	        String c;
+
+	        a=scan.nextInt();
+	        b=scan.nextDouble();
+	        scan.nextLine();
+	        c=scan.nextLine();
+	        System.out.println(i+a);
+	        System.out.println(d+b);
+	        System.out.println(s+c);
+	         scan.close();
+	}        
+}
---
+++ HackerRank_30_days_challenge/Day_1/Solution/InputString.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class InputString {
+	public static void main(String[] args) {
+       
+		Scanner scan = new Scanner(System.in); 
+
+		String inputString = scan.nextLine(); 
+		
+		scan.close(); 
+      
+		System.out.println("Hello, World.");
+        System.out.println(inputString);
+	}
+}
+
