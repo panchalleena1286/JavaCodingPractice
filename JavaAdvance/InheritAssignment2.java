29d39e1393becd9c6f05d2fdb3616944568c3b0d
panchalleena1286 <panchalleena8839@gmail.com>
2022-04-05 20:58:45 +0530

    Inheritance Concept

---
+++ JavaAdvance/InheritAssignment2.java
@@ -0,0 +1,10 @@
+
+public class InheritAssignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e = new Employee("Navin Das", 500000, 2019 , "32a4asd646d446f");
+		System.out.println(e);
+	}
+
+}
