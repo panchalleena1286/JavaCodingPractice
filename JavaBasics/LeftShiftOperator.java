be44131bf0b155e4b2c0375c31e1e2aed7be7c1f
panchalleena1286 <panchalleena8839@gmail.com>
2022-05-12 16:51:40 +0530

    java operator

---
+++ JavaBasics/LeftShiftOperator.java
@@ -0,0 +1,14 @@
+package core_java;
+
+public class LeftShiftOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 8;
+		System.out.println("The Original value of x is "+x);
+		x = x<<4;
+		System.out.println("After using <<4, the new value is "+x);
+
+	}
+
+}
