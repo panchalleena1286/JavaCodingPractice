287eed9de98466cb46839096c43add0e2dc91fff
panchalleena1286 <panchalleena8839@gmail.com>
2022-05-14 21:40:58 +0530

    java operator

---
+++ JavaBasics/RightShiftOperator.java
@@ -0,0 +1,14 @@
+package core_java;
+
+public class RightShiftOperator {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		int x = 16;
+		System.out.println("The Original value  of x is "+x);
+		x = x>>3;
+		System.out.println("After using >>3, the new value is "+x);
+	}
+
+}
