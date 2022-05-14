9a8f28538926de95a39918d8494a3a867078cdd9
panchalleena1286 <panchalleena8839@gmail.com>
2022-05-14 21:39:53 +0530

    java operator

---
+++ JavaBasics/RelationalOperator.java
@@ -0,0 +1,19 @@
+package core_java;
+
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 20;
+		int b = 10;
+		
+		System.out.println("a == b = "+ (a==b));
+		System.out.println("a != b = "+ (a!=b));
+		System.out.println("a > b = "+ (a>b));
+		System.out.println("a < b = "+ (a<b));
+		System.out.println("a >= b = "+ (a>=b));
+		System.out.println("a <= b = "+(a<=b));
+
+	}
+
+}
