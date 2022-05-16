d7230028d20ad2ff0b951031a0998de0a0b7f430
panchalleena1286 <panchalleena8839@gmail.com>
2022-05-16 23:25:45 +0530

    Create IfStatement.java

---
+++ JavaBasics/IfStatement.java
@@ -0,0 +1,14 @@
+package core_java;
+
+public class IfStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5;
+		if(x<20) {
+			System.out.print("This is if statement");
+		}
+
+	}
+
+}
