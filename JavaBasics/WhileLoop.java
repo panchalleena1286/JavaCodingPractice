f2351800442a8d8449d8ee79ffa5ba74c2016509
panchalleena1286 <panchalleena8839@gmail.com>
2022-05-17 14:59:49 +0530

    While Loop

---
+++ JavaBasics/WhileLoop.java
@@ -0,0 +1,15 @@
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		while (i < 5)
+		{
+			System.out.println("i: "+i);
+			i = i + 1;
+		}
+
+	}
+
+}
