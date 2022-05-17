d2ab33bb685185ff67e1d1074169b23a93e5d732
panchalleena1286 <panchalleena8839@gmail.com>
2022-05-17 14:57:00 +0530

    Do While Loop

---
+++ JavaBasics/DoWhileLoop.java
@@ -0,0 +1,14 @@
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 5;
+		do {
+			System.out.println("i: "+i);
+			i = i + 1;
+		}
+
+	}
+
+}
