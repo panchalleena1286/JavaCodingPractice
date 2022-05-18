1babc67c568920b4a8553d210f220104e47b05cc
panchalleena1286 <panchalleena8839@gmail.com>
2022-05-18 22:15:16 +0530

    Break statement

---
+++ JavaBasics/Break.java
@@ -0,0 +1,14 @@
+
+public class Break {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<=5; i++) {
+			if(i==2)
+				break;
+		     System.out.println("i: "+i); 
+		}
+
+	}
+
+}
