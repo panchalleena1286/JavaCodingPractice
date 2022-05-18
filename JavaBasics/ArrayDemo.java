bf3a97e8173f8410263928a66053060e4aa55ab1
panchalleena1286 <panchalleena8839@gmail.com>
2022-05-18 22:14:42 +0530

    Array

---
+++ JavaBasics/ArrayDemo.java
@@ -0,0 +1,16 @@
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] x;
+		x = new int[5];
+		x[0] = 11;
+		x[4] = 22;
+		System.out.println("Element at index 0: "+ x[0]);
+		System.out.println("Element at index 0: "+ x[1]);
+		System.out.println("Element at index 0: "+ x[4]);
+
+	}
+
+}
