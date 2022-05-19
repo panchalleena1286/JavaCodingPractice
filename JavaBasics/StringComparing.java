e6a4eb3ee4c7f166a72f2ef7ed4d007f5f4d0b0a
panchalleena1286 <panchalleena8839@gmail.com>
2022-05-19 15:48:46 +0530

    String Comparing

---
+++ JavaBasics/StringComparing.java
@@ -0,0 +1,15 @@
+
+public class StringComparing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = new String("Hello");
+		String s2 = new String("Hello");
+		if(s1==s2)
+			System.out.println("String objects referrenced are same");
+		else
+			System.out.println("String objects referrenced are not sames");
+
+	}
+
+}
