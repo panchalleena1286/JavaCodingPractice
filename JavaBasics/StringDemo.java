77f1be7fb5a6ff079dfe5869e6c0f17aed9b5b89
panchalleena1286 <panchalleena8839@gmail.com>
2022-05-19 15:47:38 +0530

    Create StringDemo.java

---
+++ JavaBasics/StringDemo.java
@@ -0,0 +1,15 @@
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Hello";
+		String s2 = "Hello";
+		if(s1==s2)
+			System.out.println("String objects referrenced are same");
+		else
+			System.out.println("String objects referrenced are not same");
+
+	}
+
+}
