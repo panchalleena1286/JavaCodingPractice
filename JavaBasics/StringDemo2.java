c0c259fcd5d153f2ef33f130e6011e2b17c41878
panchalleena1286 <panchalleena8839@gmail.com>
2022-05-19 15:48:11 +0530

    String Example

---
+++ JavaBasics/StringDemo2.java
@@ -0,0 +1,15 @@
+
+public class StringDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Hello";
+		String s2 = "Hello";
+		if(s1.contentEquals(s2))
+			System.out.println("Strings are equal");
+		else 
+			System.out.println("Strings are not equal");
+
+	}
+
+}
