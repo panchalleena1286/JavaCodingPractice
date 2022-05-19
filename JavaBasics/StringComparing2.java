29c31c799057ed97c0c6a33469ddddf8c52517f6
panchalleena1286 <panchalleena8839@gmail.com>
2022-05-19 15:49:21 +0530

    String Comparing

---
+++ JavaBasics/StringComparing2.java
@@ -0,0 +1,15 @@
+
+public class StringComparing2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = new String("Hello");
+		String s2 = new String("Hello");
+		if(s1.equals(s2))
+			System.out.println("Strings are equals");
+		else 
+			System.out.println("Strings are not equals");
+
+	}
+
+}
