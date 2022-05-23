c41f30d69c64a8e0b30678bdc26ae907e7c45430
panchalleena1286 <panchalleena8839@gmail.com>
2022-05-23 22:43:52 +0530

    String Buffer

---
+++ JavaAdvance/StringBuffer.java
@@ -0,0 +1,30 @@
+
+public class StringBuffer {
+
+	public StringBuffer(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer x = new StringBuffer("wipro");
+		x.append("Technologies");
+		System.out.println(x);
+		int i = 100;
+		x.append(i);
+		System.out.println(x);
+		
+
+	}
+
+	private void append(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void append(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
