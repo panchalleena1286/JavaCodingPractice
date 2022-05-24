8632f16fe272485becd7cb7e56ad991bb89e126e
panchalleena1286 <panchalleena8839@gmail.com>
2022-05-24 15:59:23 +0530

    Create StringBufferExample2.java

---
+++ JavaAdvance/StringBufferExample2.java
@@ -0,0 +1,16 @@
+
+public class StringBufferExample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer x = new  StringBuffer("WiproBangalore");
+		System.out.println(x);
+		char c = ' ';
+		x.insert(5,c);
+		String s = "Technologies";
+		x.insert(6,c);
+		x.insert(18,s);
+
+	}
+
+}
