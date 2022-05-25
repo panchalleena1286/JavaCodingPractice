1cae0f907ae3907d7070838967c475501e40751b
panchalleena1286 <panchalleena8839@gmail.com>
2022-05-25 22:38:58 +0530

    Create ConcatenateArray.java

---
+++ JavaBasics/ConcatenateArray.java
@@ -0,0 +1,18 @@
+
+public class ConcatenateArray {
+	public static void main(String[] args) {
+        int[] array1 = {1, 2, 3};
+        int[] array2 = {4, 5, 6};
+
+        int aLen = array1.length;
+        int bLen = array2.length;
+        int[] result = new int[aLen + bLen];
+
+        System.arraycopy(array1, 0, result, 0, aLen);
+        System.arraycopy(array2, 0, result, aLen, bLen);
+
+        Object Arrays = null;
+		System.out.println(Arrays.toString());
+    }
+
+}
