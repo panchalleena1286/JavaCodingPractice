913fa568bb4dad0dec3e5a0712bcebd348005181
panchalleena1286 <panchalleena8839@gmail.com>
2022-05-26 23:45:01 +0530

    Java Practice Program

---
+++ JavaAdvance/Assignment3/src/test1/test/Demo.java
@@ -0,0 +1,14 @@
+package test1.test;
+
+import test.Foundation;
+
+public class Demo  extends Foundation{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Foundation f = new Foundation();
+		
+		
+	}
+
+}
